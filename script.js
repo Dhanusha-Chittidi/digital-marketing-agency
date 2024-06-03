dde8f316bf92e266c00ef7a7d7eef1f7cd4ac937
Dhanusha-Chittidi <121395196+Dhanusha-Chittidi@users.noreply.github.com>
2024-06-03 16:03:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+document.getElementById('openModal').addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'block';
+});
+
+document.getElementById('closeModal').addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == document.getElementById('myModal')) {
+        document.getElementById('myModal').style.display = 'none';
+    }
+});
+// script.js
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.querySelectorAll('.slide');
+    let dots = document.querySelectorAll('.dot');
+    
+    if (n > slides.length) { slideIndex = 1 }    
+    if (n < 1) { slideIndex = slides.length }
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';  
+    }
+    
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    slides[slideIndex-1].style.display = 'block';  
+    dots[slideIndex-1].className += " active";
+}
+
+function changeImage(imageSrc) {
+    document.getElementById('project-image').src = imageSrc;
+}
+document.getElementById("contactForm").onsubmit = function() {
+    sessionStorage.setItem('formSubmitted', 'true');
+};
+
+window.onload = function() {
+    if (sessionStorage.getItem('formSubmitted') === 'true') {
+        document.getElementById("contactForm").reset();
+        sessionStorage.removeItem('formSubmitted');
+    }
+};
